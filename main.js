b418089c873ea64fb7f08a2d54f19f27a2385edf
Jose Tommaso <tommaso_j@hotmail.com>
2022-11-18 09:20:11 +0000

    Progress bar functionality

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const navbar = document.querySelector('#navbar');
 const navbarTitle = document.querySelector('#navbar h1');
 const navbarTogglerButton = document.querySelector('#navbarToggler');
 const links = document.querySelectorAll('#navbar #linksList li a');
+const progressBar = document.querySelector('#progressBar');
 
 //options for the observer
 const options = {
@@ -27,21 +28,45 @@ mainSections.forEach((section) => {
 });
 
 //navbar functionality
-
 const navbarToggler = () => {
-	const group = navbar.lastElementChild;
+	const group = document.querySelector('#navbar .group');
+
+	// toggle class to show/hide menu
 	group.classList.toggle('show');
 
+	//if element has class show, change text of button to "Hide" and change aria expanded attr to true
 	if (group.classList.contains('show')) {
 		navbarTogglerButton.innerText = 'Hide';
 		navbarTogglerButton.ariaExpanded = 'true';
 	} else {
+		//change back button to show if element has not class show and change aria expanded to false
 		navbarTogglerButton.innerText = 'Show';
 		navbarTogglerButton.ariaExpanded = 'false';
 	}
 };
 
+//add click event to every link in the navbar menu so it close once its clicked
 links.forEach((link) => {
 	link.addEventListener('click', navbarToggler);
 });
+
+//add click event to the navbar button to open/close navbar menu
 navbarTogglerButton.addEventListener('click', navbarToggler);
+
+// function to update progress bar
+const progressBarUpdater = () => {
+	//gets number of pixels from the top of viewport and the scroll position
+	const { scrollTop } = document.documentElement;
+
+	//get the height of the mainSections element
+	const { clientHeight } = mainSections[0].parentElement;
+
+	const scrollPercent = `${
+		(scrollTop / (clientHeight + window.innerHeight)) * 100
+	}%`;
+
+	progressBar.style.width = scrollPercent;
+};
+
+//add scroll event to update the progress bar
+document.addEventListener('scroll', progressBarUpdater);
